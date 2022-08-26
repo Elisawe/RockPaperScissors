a3e12109cd189c28ca33823e78638e249c3ad98f
Elisawe <kononova.2000@mail.ru>
2022-08-26 18:04:38 +0500

    add basic UI

--- rps.js
+++ rps.js
@@ -1,56 +1,79 @@
-let playerScore = 0
-let computerScore = 0
-const buttons = document.querySelectorAll('input')
+let computerScore = 0;
+let playerScore = 0;
 
-function computerPlay() {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-}
 
-function disableButtons() {
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
-}
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click',play);
 
-function playRound(playerSelection) {
-    let computerSelection = computerPlay()
-    let result = ""
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click',play);
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper') ||
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click',play);
+
+const div = document.querySelector('#results');
+
+function play(e){
+    let compMove = computerPlay();
+    let status =  playRound(this.id,compMove);
+    let p = document.createElement('p');
+    if(status == 'lose'){
+        computerScore += 1;
+        p.textContent = 'You lose! ' + compMove + ' beats ' + this.id +'        Score-> Comp: ' + computerScore + ' Player: ' + playerScore + '\n';
         
-        playerScore += 1
-        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-
-        if (playerScore == 5) {
-            result += '<br><br>You won the game! Reload the page to play again'
-            disableButtons()
-        }
-    }
-    else if (playerSelection == computerSelection) {
-        result = ('It\'s a tie. You both chose ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-    }
-    else {
-        computerScore += 1
-        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
-            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
-
-        if (computerScore == 5) {
-            result += '<br><br>I won the game! Reload the page to play again'
-            disableButtons()
-        }
+        
+    }else if(status == 'win'){
+        playerScore += 1;
+        p.textContent = 'You win! ' + this.id + ' beats ' + compMove +'     Score-> Comp: ' + computerScore + ' Player: ' + playerScore + '\n';
+    }else{
+        p.textContent = status +'\nScore->\tComp: ' + computerScore + ' Player: ' + playerScore + '\n';
     }
 
-    document.getElementById('result').innerHTML = result
-    return
+    div.appendChild(p);
+
+    if(computerScore == 5){
+        const result = document.createElement('p');
+        result.textContent = 'YOU HAVE LOST THE GAME!!!';
+        div.appendChild(result);
+        computerScore = 0;
+        playerScore = 0;
+    }else if(playerScore == 5){
+        const result = document.createElement('p');
+        result.textContent = 'YOU HAVE WON THE GAME!!!';
+        div.appendChild(result);
+        computerScore = 0;
+        playerScore = 0;
+    }
+    
+    
 }
 
-buttons.forEach(button =>{
-    button.addEventListener('click', function(){
-        playRound(button.value)
-    })
-})
+function computerPlay(){
+    let ran = Math.random()*3;
+    if(ran<=1){
+        return 'rock';
+    }else if(ran <=2){
+        return 'paper';
+    }else{
+        return 'scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(computerSelection==playerSelection){
+        return "It's a tie!";
+    }else if(computerSelection == 'rock' && playerSelection == 'paper'){
+        return "win";
+    }else if(computerSelection == 'paper' && playerSelection == 'rock'){
+        return "lose";
+    }else if(computerSelection == 'paper' && playerSelection == 'scissors'){
+        return "win";
+    }else if(computerSelection == 'scissors' && playerSelection == 'paper'){
+        return "lose";
+    }else if(computerSelection == 'scissors' && playerSelection == 'rock'){
+        return "win";
+    }else if(computerSelection == 'rock' && playerSelection == 'scissors'){
+        return "lose";
+    }
+}
